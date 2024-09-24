79664671998b2222169a6f32d936ffc1d960e953
Izanalgit <izanalvarez@gmail.com>
2024-09-24 21:00:32 +0200

    update : encrypt implementation on message model

--- models/Message.js
+++ models/Message.js
@@ -1,23 +1,44 @@
 const mongoose = require('mongoose');
+const {encryptText,decryptText} = require('../utils/messageCrypto');
 
 const messageSchema = new mongoose.Schema({
   remit: {
     type: mongoose.Schema.ObjectId,
-    required: true,
+    required: true  
   },
   recep: {
     type: mongoose.Schema.ObjectId,
-    required: true,
+    required: true
   },
   messageText: {
     type: String,
-    required: true,
+    required: true
+  },
+  iv:{
+    type: String,
+    required: true
   },
   createdAt: { 
     type: Date, 
     default: Date.now 
 }
 });
+
+//Encrypt/Decrypt messages
+
+messageSchema.pre('save', (next) => {
+  const message = this;
+
+  const {encryptedData, iv} = encryptText(message.messageText);
+  message.messageText = encryptedData;
+  message.iv = iv;
+
+  next();
+})
+
+messageSchema.methods.decryptMessage = () => {
+  return decryptText(this.messageText, this.iv);
+}
 
 const Message = mongoose.model('Message', messageSchema);
 module.exports = Message;
