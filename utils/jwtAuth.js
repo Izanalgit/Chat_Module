d7127a68401d612175cfbce4f8d5da15c783f829
Izanalgit <izanalvarez@gmail.com>
2024-09-26 18:05:46 +0200

    fix : jwt secret variable grammar

--- utils/jwtAuth.js
+++ utils/jwtAuth.js
@@ -3,7 +3,7 @@ const jwt = require ('jsonwebtoken');
 //Token generator
 function genToken(user){
     const payload = {user: user._id};
-    return jwt.sign(payload,process.env.JWT_SECRECT,{expiresIn:'2h'});
+    return jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:'2h'});
 }
 
 module.exports = {genToken}
