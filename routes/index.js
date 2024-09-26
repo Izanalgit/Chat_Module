84dce0d13b65af3eb02f02b510b5cbd845e977e7
Izanalgit <izanalvarez@gmail.com>
2024-09-26 14:05:34 +0200

    feat : block user routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/user', require('./userRoutes'));
-router.use('/chat', require('./messageRoute'));
+router.use('/chat', require('./messageRoutes'));
+router.use('/privacy', require('./privacyRoutes'));
 
 module.exports = router;
